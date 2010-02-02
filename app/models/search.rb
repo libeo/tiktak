07dad1c9fc69d6b50eb83ef7cd65a14a517837b5
Gregory Eric Sanderson <gregory@sys-tech.net>
2010-02-02 17:06:43 -0500

    forgot to merge Search model

--- app/models/search.rb
+++ app/models/search.rb
@@ -4,30 +4,44 @@ class Search
   # in a find.
   # When used in a find, the  id and any given fields 
   # will be searched and any objects with a ANY of the 
-  # given strings as a substring will be returned.
+  # given strings as a starting substring will be returned.
   # If a number is given, any objects with that id will be 
   # returned, but so will any objects with that number in fields.
+  #
+  # If options[:search_by_id] is false, ids won't be searched automatically.
+  # If options[:start_search_only], only values starting with the given string will be returned.
+  
   ###
-  def self.search_conditions_for(strings, fields = [ :name ])
+  def self.search_conditions_for(strings, fields = [ :name ], options = {})
+    search_by_id = options.has_key?(:search_by_id) ? options[:search_by_id] : true
+
     conds = []
     cond_params = []
     
-    strings.each do |s|
-      next if s.to_i <= 0
-      conds << "id = ?"
-      cond_params << s
+    if search_by_id
+      strings.each do |s|
+        next if s.to_i <= 0
+        conds << "id = ?"
+        cond_params << s
+      end
     end
     
     fields.each do |field|
       strings.each do |s|
         next if s.strip.blank?
         conds << "lower(#{ field }) like ?"
-        cond_params << "%#{ s.downcase.strip }%"
+        if options[:start_search_only]
+          cond_params << "#{ s.downcase.strip }%"
+        else
+          cond_params << "%#{ s.downcase.strip }%"
+        end
       end
     end
 
     if conds.any?
-      return [ conds.join(" or ") ] + cond_params
+      full_conditions = [ conds.join(" or ") ] + cond_params
+      sanitized = ActiveRecord::Base.send(:sanitize_sql_array, full_conditions)
+      return "(#{ sanitized })"
     end
   end
 end
