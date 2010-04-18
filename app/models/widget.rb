689bf131a6ca82d109132a1ecd43b90059488476
Gregory Eric Sanderson <gzou2000@gmail.com>
2010-04-18 12:50:12 -0400

    WIP, need to check things in master

--- app/models/widget.rb
+++ app/models/widget.rb
@@ -8,7 +8,7 @@ class Widget < ActiveRecord::Base
 
   validates_presence_of :name
 
-  def name
+  def name_and_type
     res = ""
     if self.filter_by && self.filter_by.length > 0
       begin
@@ -31,6 +31,32 @@ class Widget < ActiveRecord::Base
     end 
     res << " [#{_'Mine'}]" if self.mine?
     "#{@attributes['name']}#{ res.empty? ? "" : " - #{res}"}"
+  end
+
+
+  def type_text
+    res = ""
+    if self.filter_by && self.filter_by.length > 0
+      begin
+        res << case self.filter_by[0..0]
+               when 'c'
+                 User.find(self.user_id).company.customers.find(self.filter_by[1..-1]).name
+               when 'p'
+                 User.find(self.user_id).projects.find(self.filter_by[1..-1]).name
+               when 'm'
+                 m = Milestone.find(self.filter_by[1..-1], :conditions => ["project_id IN (#{User.find(self.user_id).projects.collect(&:id).join(',')})"])
+                 "#{m.project.name} / #{m.name}"
+               when 'u'
+                 _('[Unassigned]')
+               else 
+                 ""
+               end 
+      rescue
+        res << _("Invalid Filter")
+      end 
+    end 
+      res << " [#{_'Mine'}]" if self.mine?
+      return res
   end
 
   def order_by_sql
