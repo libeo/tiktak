bbb11b5b207ffffa4e07a3283383617345302231
Gregory Eric Sanderson <gregory@sys-tech.net>
2010-05-05 14:44:40 -0400

    reorganised javascript and css packaging

--- public/javascripts/task_filter.js
+++ public/javascripts/task_filter.js
@@ -1,7 +1,6 @@
 function toggleFilterPanel(link, cookie) {
-  link = jQuery(link);
-  parent().next().toggle(500, function(){
-      if (!jQuery.cookie(cookie) == 'true') {
+  jQuery(link).parent().next().toggle(500, function(){
+      if (jQuery.cookie(cookie) == 'false') {
           jQuery.cookie(cookie, 'true');
       } else {
           jQuery.cookie(cookie, 'false');
