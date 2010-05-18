d0305af12516cb5a3fc99a2575a12fb2bcf60e30
Gregory Eric Sanderson <gregory@sys-tech.net>
2010-05-18 16:32:55 -0400

    removed worklog creation when modifying pages. Made too many errors

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -20,19 +20,6 @@ class PagesController < ApplicationController
     @page.user = current_user
     @page.company = current_user.company
     if((@page.project_id.to_i > 0) && @page.save )
-      worklog = WorkLog.new
-      worklog.user = current_user
-      worklog.project = @page.project
-      worklog.company = @page.project.company
-      worklog.customer = @page.project.customer
-      worklog.body = "#{@page.name}"
-      worklog.task_id = 0
-      worklog.started_at = Time.now.utc
-      worklog.duration = 0
-      worklog.log_type = EventLog::PAGE_CREATED
-      worklog.body = "- #{@page.name} Created"
-      worklog.save
-
       flash['notice'] = _('Note was successfully created.')
       redirect_to :action => 'show', :id => @page.id
     else
@@ -52,22 +39,6 @@ class PagesController < ApplicationController
     old_project = @page.project_id
 
     if @page.update_attributes(params[:page])
-
-      worklog = WorkLog.new
-      worklog.user = current_user
-      worklog.project = @page.project
-      worklog.company = @page.project.company
-      worklog.customer = @page.project.customer
-      worklog.body = "#{@page.name}"
-      worklog.task_id = 0
-      worklog.started_at = Time.now.utc
-      worklog.duration = 0
-      worklog.log_type = EventLog::PAGE_MODIFIED
-      worklog.body = ""
-      worklog.body << "- #{old_name} -> #{@page.name}\n" if old_name != @page.name
-      worklog.body << "- #{@page.name} Modified\n" if old_body != @page.body
-      worklog.save
-
       flash['notice'] = _('Note was successfully updated.')
       redirect_to :action => 'show', :id => @page
     else
@@ -77,20 +48,6 @@ class PagesController < ApplicationController
 
   def destroy
     @page = Page.find(params[:id], :conditions => ["company_id = ?", current_user.company.id] )
-
-    worklog = WorkLog.new
-    worklog.user = current_user
-    worklog.project = @page.project
-    worklog.company = @page.project.company
-    worklog.customer = @page.project.customer
-    worklog.body = "#{@page.name}"
-    worklog.task_id = 0
-    worklog.started_at = Time.now.utc
-    worklog.duration = 0
-    worklog.log_type = EventLog::PAGE_DELETED
-    worklog.body = "- #{@page.name} Deleted"
-    worklog.save
-
     @page.destroy
     redirect_to :controller => 'tasks', :action => 'list'
   end
